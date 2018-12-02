55088ca104d65a9e017c1471a19ff0098a7510d7
russnmurray <russnmurray@gmail.com>
2018-12-02 13:54:43 -0500

    Add files via upload

---
+++ testapp-duplicate.js
@@ -0,0 +1,8 @@
+require("riverbed-appinternals")({debug:true});
+
+var http = require('http');
+
+http.createServer(function(req,resp){
+        resp.writeHead(200,{'Content-Type':'text/plain'});
+        resp.end("Hello World");
+}).listen(9998);
